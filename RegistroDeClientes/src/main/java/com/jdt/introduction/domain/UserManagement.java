d88a4ee2d91a57ec5bed0f532686159ba6271a48
Jhojan Toro-Study <jhojan1806.study@gmail.com>
2022-11-09 14:26:18 -0500

    User management was done

---
+++ RegistroDeClientes/src/main/java/com/jdt/introduction/domain/UserManagement.java
@@ -0,0 +1,91 @@
+
+package com.jdt.introduction.domain;
+
+public class UserManagement {
+    private static int id;
+    private static int identificationCard;
+    private static String name;
+    private static String email;
+    private static int phone;
+
+    public UserManagement() {
+    }
+    
+    public UserManagement(int id) {
+        this.id = id; 
+    }
+        
+    public UserManagement(int id, int identificationCard, String name,String email, int phone) {
+        this.id = id; 
+        this.identificationCard = identificationCard;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public UserManagement(int identificationCard, String name,String email, int phone) {
+        
+        this.identificationCard = identificationCard;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        UserManagement.id = id;
+    }
+
+    public static int getIdentificationCard() {
+        return identificationCard;
+    }
+
+    public static void setIdentificationCard(int identificationCard) {
+        UserManagement.identificationCard = identificationCard;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        UserManagement.name = name;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        UserManagement.email = email;
+    }
+
+    public static int getPhone() {
+        return phone;
+    }
+
+    public static void setPhone(int phone) {
+        UserManagement.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("UserManagement{");
+        sb.append("id:"+id);
+        sb.append(" Cedula:"+identificationCard);
+        sb.append(" Nombre:"+name);
+        sb.append(" Correo Electronico:"+email);
+        sb.append(" Telefono:"+phone);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    
+
+    
+    
+}
